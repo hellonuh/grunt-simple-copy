08e313d1c60c0b67337bc2e46bcecf750ce7e9e5
Neyazul Haque <nuhaque@gmail.com>
2017-11-26 14:35:18 +0530

    Added proper test

--- test/simple_copy_test.js
+++ test/simple_copy_test.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const grunt = require('grunt');
+const dircompare = require('dir-compare');
 
 exports.simple_copy = {
   setUp: function(done) {
     done();
   },
-  type1Copy: function(test) {
+  main: function(test) {
     test.expect(1);
     const actual = true;
-    const expected = true
+    const expected = (() =>{
+      try{
+        var res = dircompare.compareSync('test/fixtures/src', 'tmp/dist', {compareContent:true});
+        if(res.same){
+          return true
+        }
+        return false;
+      } catch(err) {
+        console.log(err);
+        return false;
+      }
+      return true;
+    })()
     test.equal(actual, expected, 'Test if type1 copy works');
     test.done();
   },
